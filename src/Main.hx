5d9ffc1ed363ac36a4ee95ccb367427aa17af382
fuzhou.chen <fuzhouch@gmail.com>
2023-12-09 01:20:22 +0800

    [Task 0006] Basic tile moving logic.
    Implement a simple tile moving logic with a simple gravity/velocity
    computation.

--- src/Main.hx
+++ src/Main.hx
@@ -1,4 +1,11 @@
 class Main extends hxd.App {
+    var bmp: h2d.Bitmap;
+    var tile: h2d.Tile;
+
+    var velocity: Float = 0;
+    var gravity: Float = 9.8;
+    var unitSize: Float = 64;
+
     override function init() {
         var window = hxd.Window.getInstance();
         window.title = "Claw Machine Adventure";
@@ -8,8 +15,20 @@ class Main extends hxd.App {
         // either hardcoding, or read from configuration file.
         window.resize(1280, 720); 
 
-        var tf = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
-        tf.text = "Hello, Hashlink !";
+
+        tile = h2d.Tile.fromColor(0xFF0000, 64, 64).center();
+        bmp = new h2d.Bitmap(tile, s2d);
+        bmp.x = s2d.width * 0.5;
+        bmp.y = s2d.height * 0.5;
+    }
+
+    override function update(dt: Float) {
+        bmp.y += velocity * unitSize * dt;
+        velocity += gravity * dt;
+
+        if (bmp.y + bmp.tile.height / 2 > s2d.height) {
+            velocity = -velocity;
+        }
     }
 
     static function main() {
